c5513fe596ada930a28b73dafea008a6f2f49035
Pavel Druzhinin <paveldroo@gmail.com>
2024-01-30 11:25:08 +0300

    Add static fileserver

--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", index)
-	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", http.FileServer(http.Dir("."))))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	html := `<h1>
-				<body>
-					<p>Here is a photo:</p>
-					<img src="/resources/doggy.jpg" />
-				</body>
-			</h1>`
+// Here is an old manual fileserver
 
-	io.WriteString(w, html)
-}
-
-func serve(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "doggy.jpg")
-}
+//func main() {
+//	http.HandleFunc("/", index)
+//	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+//	log.Fatal(http.ListenAndServe(":8000", nil))
+//}
+//
+//func index(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+//	html := `<h1>
+//				<body>
+//					<p>Here is a photo:</p>
+//					<img src="/resources/doggy.jpg" />
+//				</body>
+//			</h1>`
+//
+//	io.WriteString(w, html)
+//}
+//
+//func serve(w http.ResponseWriter, r *http.Request) {
+//	http.ServeFile(w, r, "doggy.jpg")
+//}
